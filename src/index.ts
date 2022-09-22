286aaa30565d3e8612b1c80c765125f27495e92f
Matthias Kay <github@matthiaskay.de>
2022-09-22 14:28:46 +0200

    x

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("Hello world");
+import { CommandLineInterface } from "./adapter/endpoint/CommandLineInterface";
+
+// for simplicity
+new CommandLineInterface().run();
---
+++ tests/dummy.spec.ts
@@ -0,0 +1,5 @@
+describe("Dummy", () => {
+  it("just do it", () => {
+    expect(true).toBeTruthy();
+  });
+});
