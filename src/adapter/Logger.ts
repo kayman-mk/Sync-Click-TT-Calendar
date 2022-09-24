25fabfe35b210c7ffef98cfe5853bad18da2e18e
Matthias Kay <github-public@matthiaskay.de>
2022-09-24 15:24:09 +0200

    chore: improve log output (#15)
    # Description
    
    Configure Winston to use pretty print, timestamps and print stack
    traces. Also catch all uncaught exceptions and rejections.
    
    # Checklist
    
    - [x] My code follows the style guidelines of the project
    - [x] I have performed a self-review of my own code
    - [ ] I have made corresponding changes to the documentation

--- src/adapter/Logger.ts
+++ src/adapter/Logger.ts
@@ -1,4 +1,5 @@
-import winston from 'winston';
+
+import winston, { format } from 'winston';
 import { Configuration } from "./Configuration";
 
 export class Logger {
@@ -6,10 +7,37 @@ export class Logger {
 
     constructor(configuration: Configuration) {
         this.logger = winston.createLogger({
+            format: format.combine(format.errors({ stack: true }), format.timestamp(), format.prettyPrint()),
+            level: 'info',
             transports: [
                 new winston.transports.Console(),
             ],
-            
+            exceptionHandlers: [
+                new winston.transports.Console(),
+              ],
+              rejectionHandlers: [
+                new winston.transports.Console(),
+              ],
         });
+    }
+
+    error(message: any) {
+        this.logger.error(message);
+    }
+
+    warning(message: any) {
+        this.logger.warning(message);
+    }
+
+    info(message: any) {
+        this.logger.info(message);
+    }
+
+    debug(message: any) {
+        this.logger.verbose(message);
+    }
+
+    trace(message: any) {
+        this.logger.debug(message);
     }
 }
