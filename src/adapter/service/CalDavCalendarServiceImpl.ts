a852e5c3f2005a870524b5984d42c695c6b8ad64
Matthias Kay <github@matthiaskay.de>
2022-09-28 20:50:21 +0200

    x

--- src/adapter/service/CalDavCalendarServiceImpl.ts
+++ src/adapter/service/CalDavCalendarServiceImpl.ts
@@ -1,9 +1,9 @@
 import { Appointment } from "../../domain/model/appointment/Appointment";
 import { CalendarService } from "../../domain/service/CalendarService";
 
-export class WebCalCalendarServiceImpl implements CalendarService {
+export class CalDavCalendarServiceImpl implements CalendarService {
     constructor() {}
-    
+
     downloadAppointments(): Set<Appointment> {
         throw new Error("Method not implemented.");
     }
