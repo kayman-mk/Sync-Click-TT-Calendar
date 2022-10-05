87179da3a853ce9131e021cabb02ba1a3fb1c3e1
Matthias Kay <github@matthiaskay.de>
2022-10-05 21:19:44 +0200

    x

--- tests/domain/model/appointment/Appointment.spec.ts
+++ tests/domain/model/appointment/Appointment.spec.ts
@@ -1,12 +1,12 @@
 import {Appointment, AppointmentFactory} from "../../../../src/domain/model/appointment/Appointment";
 
 describe('Appointment Factory', () => {
-    it('should have a title equal to "local - foreign" team name', () => {
+    it('should have a title equal to "local - foreign (age)" team name', () => {
         // when
         const actualAppointment: Appointment = AppointmentFactory.create('local', 'remote', '01.01.1970 00:00', '3. KK West', 5, 'location', 'age', true);
 
         // then
-        expect(actualAppointment.title).toEqual("local - remote");
+        expect(actualAppointment.title).toEqual("local - remote (age)");
     })
 
     it('should parse the start date time', () => {
