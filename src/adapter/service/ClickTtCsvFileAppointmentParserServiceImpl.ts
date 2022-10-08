cb968cae91ac7b070ee43063e5174c43107a09db
Matthias Kay <github-public@matthiaskay.de>
2022-10-08 11:06:47 +0200

    test: test the CSV parser (#43)
    # Description
    
    Add some tests to check the CSV parser.
    
    # Checklist
    
    - [x] My code follows the style guidelines of the project
    - [x] I have performed a self-review of my own code
    - [ ] I have made corresponding changes to the documentation

--- src/adapter/service/ClickTtCsvFileAppointmentParserServiceImpl.ts
+++ src/adapter/service/ClickTtCsvFileAppointmentParserServiceImpl.ts
@@ -7,6 +7,53 @@ import { Logger } from '../Logger';
 import { FileStorageService } from '../../domain/service/FileStorageService';
 import { Readable } from 'stream';
 import { DateTimeFormatter, LocalDateTime } from '@js-joda/core';
+import { type } from 'os';
+
+/**
+ * Specification of the CSV file which can be downloaded from Click-TT
+ */
+type ClickTtCsvFile = {
+    /**
+     * The age group the teams are assigned to
+     */
+    Altersklasse: string
+    /**
+     * Number of the game in the league of the season.
+     */
+    BegegnungNr: number
+    /**
+     * Name of the foreign team
+     */
+    GastMannschaft: string
+    /**
+     * In case no match is planned for one round it is indicated with "spielfrei"
+     */
+    GastVereinName: "spielfrei" | string
+    /**
+     * Names the complete address: HalleName, HalleStrasse, HallePLZ and HalleOrt
+     */
+    HalleName: string
+    HallePLZ: string
+    HalleOrt: string
+    HalleStrasse: string
+    /**
+     * Name of the local team
+     */
+    HeimMannschaft: string
+    /**
+     * Indicating if it is a match belonging to the cup, the first half of the season (VR) or
+     * the last half of the season (RR).
+     */
+    Runde: "Pokal" | "VR" | "RR"
+    /**
+     * A division within the league
+     */
+    Staffel: string
+    /**
+     * The start date time in local timezone
+     */
+    Termin: string
+}
 
 @injectable()
 export class ClickTtCsvFileAppointmentParserServiceImpl implements AppointmentParserService {
@@ -14,18 +61,21 @@ export class ClickTtCsvFileAppointmentParserServiceImpl implements AppointmentPa
 
     async parseAppointments(filename: string): Promise<Set<AppointmentInterface>> {
         const appointments: Set<AppointmentInterface> = new Set();
-        const formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        const csvDateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
         return new Promise((resolve, reject) => {
             Readable.from(this.fileStorageService.readFile(`${filename}`))
                 .pipe(csv({ separator: ';' }))
-                .on('data', (data) => {
+                .on('data', (data: ClickTtCsvFile) => {
+                    const startDateTime = LocalDateTime.parse(data.Termin, csvDateTimeFormatter)
+
                     if (data.GastVereinName != 'spielfrei') {
-                        const startDateTime = LocalDateTime.parse(data.Termin, formatter)
                         const location = data.HalleName + ", " + data.HalleStrasse + ", " + data.HallePLZ + " " + data.HalleOrt;
                         const isCup = data.Runde == 'Pokal'
 
                         appointments.add(AppointmentFactory.createFromCsv(data.HeimMannschaft, data.GastMannschaft, startDateTime, data.Staffel, data.BegegnungNr, location, data.Altersklasse, isCup));
+                    } else {
+                        this.logger.info("Appointment ignored. It's marked with 'spielfrei' " + startDateTime)
                     }
                 })
                 .on('end', () => {
---
+++ tests/adapter/service/ClickTtCsvFileAppointmentParserServiceImpl.spec.ts
@@ -0,0 +1,58 @@
+import { LocalDateTime } from "@js-joda/core"
+import { Readable } from "stream"
+import { Logger } from "../../../src/adapter/Logger"
+import { ClickTtCsvFileAppointmentParserServiceImpl } from "../../../src/adapter/service/ClickTtCsvFileAppointmentParserServiceImpl"
+import { FileStorageService } from "../../../src/domain/service/FileStorageService"
+
+class TestFileStorageService implements FileStorageService {
+    readFile(filename: string): Buffer {
+        const csvFile = `Termin;Staffel;Runde;HeimMannschaft;HalleStrasse;HalleOrt;HallePLZ;HalleName;GastVereinName;GastMannschaft;BegegnungNr;Altersklasse
+10.10.2022 11:45;3. KK West;VR;VfL Hamburg;Lübecker Straße 4;Lübeck;23456;Holstenhalle;SC Lübeck;SC Lübeck III;2;Herren
+11.10.2022 11:45;3. KK West;VR;VfL Hamburg;Lübecker Straße 4;Lübeck;23456;Holstenhalle;spielfrei;;2;Herren`
+
+        return Buffer.from(csvFile)
+    }
+}
+
+class TestLogger extends Logger {
+}
+
+describe('Parse Click-TT CSV file', () => {
+    it('should create appointment from CSV data', () => {
+        const parser = new ClickTtCsvFileAppointmentParserServiceImpl(new TestFileStorageService(), new TestLogger())
+
+        // when
+        parser.parseAppointments("abc.csv").then(actualAppointments => {
+            // then
+            expect(actualAppointments.size).toEqual(1);
+        })
+    })
+
+    it('should ignore "spielfrei" appointments', () => {
+        const parser = new ClickTtCsvFileAppointmentParserServiceImpl(new TestFileStorageService(), new TestLogger())
+
+        // when
+        parser.parseAppointments("abc.csv").then(actualAppointments => {
+            // then
+            expect(actualAppointments.size).toEqual(1);
+        })
+    })
+
+    it('should extract necessary data from CSV data', () => {
+        const parser = new ClickTtCsvFileAppointmentParserServiceImpl(new TestFileStorageService(), new TestLogger())
+
+        // when
+        parser.parseAppointments("abc.csv").then(actualAppointments => {
+            const actualAppointment = actualAppointments.values().next().value
+
+            // then
+            expect(actualAppointment.id).toEqual("ID: 3. KK West-2");
+            expect(actualAppointment.ageClass).toEqual("Herren");
+            expect(actualAppointment.categories).toEqual(["Click-TT", "Herren", "Liga"]);
+            expect(actualAppointment.isCup).toEqual(false);
+            expect(actualAppointment.location).toEqual("Holstenhalle, Lübecker Straße 4, 23456 Lübeck");
+            expect(actualAppointment.startDateTime).toEqual(LocalDateTime.parse("2022-10-10T11:45"));
+            expect(actualAppointment.title).toEqual("VfL Hamburg - SC Lübeck III (Herren)");
+        })
+    })
+})
